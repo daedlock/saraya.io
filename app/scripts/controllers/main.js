d7a9a8e2d06da15840cdebfe9f7760ed5eee2057
Hossam Saraya <hossam.saraya@gmail.com>
2015-04-14 00:47:09 +0200

    header

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,14 +9,19 @@
  */
 angular.module('hsApp')
   .controller('MainCtrl', function ($scope) {
+
+    $scope.minHeight = {
+      'min-height': $(document).height() + "px"
+    };
+
+
     $scope.load = function(){
-      $scope.minHeight = {
-        'min-height': $(document).height() + "px"
-      };
+
+
+
       $(document).ready(function(){
         $(".home-wrapper .header").backstretch("/images/desk_bg.png");
         $(".home-wrapper .footer").backstretch("/images/footer_bg.png");
-        console.log($(document));
       });
     };
 
